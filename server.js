dcf5e3ce1aa8aeacb946ee083645940b86a86d48
NEXUS <publicbot34@gmail.com>
2024-08-02 23:03:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,22 +2,29 @@ const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysocket
 const qrcode = require('qrcode-terminal');
 
 async function connectToWhatsApp() {
-    const { state, saveCreds } = await useMultiFileAuthState('./auth_info');
+    // Use a specific directory for multi-device authentication state
+    const { state, saveCreds } = await useMultiFileAuthState('./auth_info_multi_device');
+    
     const sock = makeWASocket({
         auth: state,
+        printQRInTerminal: true, // Automatically print QR code in terminal
     });
 
     sock.ev.on('creds.update', saveCreds);
+
     sock.ev.on('connection.update', (update) => {
         const { qr, connection, lastDisconnect } = update;
+
         if (qr) {
+            console.log('Scan the QR code with WhatsApp to login:');
             qrcode.generate(qr, { small: true });
         }
+
         if (connection === 'close') {
-            if (lastDisconnect.error) {
+            const shouldReconnect = lastDisconnect?.error != null;
+            if (shouldReconnect) {
                 console.log('Connection closed due to an error. Attempting to reconnect...');
-                // Optionally, you can add a delay before reconnecting to avoid rapid reconnect attempts
-                setTimeout(() => connectToWhatsApp(), 5000);
+                setTimeout(connectToWhatsApp, 5000); // Reconnect with a delay
             } else {
                 console.log('Connection closed. You need to scan the QR code again.');
             }
@@ -33,4 +40,4 @@ async function connectToWhatsApp() {
     return sock; // Return the socket instance
 }
 
-module.exports = connectToWhatsApp;  // Export the function
+module.exports = connectToWhatsApp;
